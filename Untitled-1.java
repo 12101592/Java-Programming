22aea3ceb59049477c0333a019e5c5fac4c6b171
Mohd Afzal Raza <94395127+12101592@users.noreply.github.com>
2023-04-24 14:09:27 +0530

    Add files via upload

--- Untitled-1.java
+++ Untitled-1.java
@@ -1,26 +1,12 @@
+ 
+
+// write a program to find factorial of number?
 
 
 
-
-import java.util.Scanner;
-class new_me{
-
+ class ew_me{
 
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-
-        String s= sc.nextLine();
-        char c= sc.next().charAt(0);
-
-int count=0;
- for(int i=0;0<s.length();i++){
-if(s.charAt(i)==c){
-    count++;
-}
-
- }
- System.out.print(count);
-
-        
+        System.out.println("null");
     }
-}
+ }
